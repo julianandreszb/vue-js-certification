e427b29710439797e58e0f40282219bd6423387c
Julian Zapata <julianandreszb@gmail.com>
2024-05-09 01:27:47 -0500

    feat: Dynamically Binding Multiple Attributes

---
+++ src/components/BindingMultipleAttributes.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { reactive } from 'vue'
+
+const objectOfAttributes = reactive({
+  value: 'Hello World', // value attribute is not bind to the input element
+  class: 'text-red',
+  id: 'unique-id',
+})
+
+</script>
+
+<template>
+  <input v-bind="objectOfAttributes" />
+  <!-- RENDER:  -->
+  <!-- <input class="text-red" id="unique-id">  -->
+  
+  
+  <!-- SAME THAT -->
+  <input :value="objectOfAttributes.value" :class="objectOfAttributes.class" :id="objectOfAttributes.id" />
+  <!-- RENDER:  -->
+  <!-- <input class="text-red" id="unique-id">  -->
+</template>
+
+<style scoped>
+.text-red {
+  color: red;
+}
+</style>
