46ae47602f8cff1e4e020796de1ebd24d69e46e2
Julian Zapata <julianandreszb@gmail.com>
2024-05-09 02:15:36 -0500

    feat: Dynamic Arguments

--- src/components/DynamicArguments.vue
+++ src/components/DynamicArguments.vue
@@ -1,12 +1,17 @@
 <script setup>
-const attributeRefValue = 'https://www.google.com'
+
 const attributeRef = 'href'
+const attributeRefValue = 'https://www.google.com'
+
 const attributeTarget = 'target'
 const attributeTargetValue = '_blank'
 </script>
 
 <template>
   <a v-bind:[attributeRef]="attributeRefValue" v-bind:[attributeTarget]="attributeTargetValue" >It should open Google in a new tab!</a>
+
+  <!-- SAME THAT -->
+  <a :[attributeRef]="attributeRefValue" :[attributeTarget]="attributeTargetValue" >It should open Google in a new tab!</a>
 </template>
 
 <style scoped></style>
