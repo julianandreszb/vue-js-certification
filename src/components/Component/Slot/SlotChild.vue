8d898e634a30e315223b853f92b0de8820caf475
Julian Zapata <julianandreszb@gmail.com>
2024-06-08 14:42:33 -0500

    Save progress: Default slot

---
+++ src/components/Component/Slot/SlotChild.vue
@@ -0,0 +1,17 @@
+<script setup></script>
+
+<template>
+  <article>
+    <slot></slot>
+  </article>
+</template>
+
+<style scoped>
+article {
+  border: 1px solid red;
+  width: 200px;
+  height: 200px;
+  margin: 10px;
+  padding: 10px;
+}
+</style>
---
+++ src/components/Component/Slot/SlotComponentContainer.vue
@@ -0,0 +1,13 @@
+<script setup>
+import SlotChild from '@/components/Component/Slot/SlotChild.vue'
+</script>
+
+<template>
+  <SlotChild>
+    <div :style="{ backgroundColor: 'red' }">
+      <h1>SlotChild</h1>
+    </div>
+  </SlotChild>
+</template>
+
+<style scoped></style>
