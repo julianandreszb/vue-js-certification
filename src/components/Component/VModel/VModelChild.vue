90b421b9415364f3ba744b306314b528eb83ae58
Julian Zapata <julianandreszb@gmail.com>
2024-06-13 03:51:59 -0500

    Save progress: V-model (Multi-model)

--- src/components/Component/VModel/VModelChild.vue
+++ src/components/Component/VModel/VModelChild.vue
@@ -1,6 +1,8 @@
 <script setup>
 const counter = defineModel()
 
+const firstName = defineModel('firstName')
+
 function increment() {
   counter.value++
 }
@@ -9,6 +11,10 @@ function increment() {
 <template>
   <button @click="increment">Increase Counter</button>
   <p>Counter on child {{ counter }}</p>
+
+  <hr>
+  <label for="firstNameInput">First Name</label>
+  <input id="firstNameInput" type="text" v-model="firstName" >
 </template>
 
 <style scoped></style>
--- src/components/Component/VModel/VModelParent.vue
+++ src/components/Component/VModel/VModelParent.vue
@@ -2,12 +2,14 @@
 import VModelChild from '@/components/Component/VModel/VModelChild.vue'
 import { ref } from 'vue'
 const counter = ref(0)
+const userName = ref('Julian')
 </script>
 
 <template>
   <h1>v-model</h1>
   <p>Counter on parent: {{ counter }}</p>
-  <VModelChild v-model="counter" />
+  <p>User Name: {{ userName }}</p>
+  <VModelChild v-model="counter" v-model:first-name="userName" />
 </template>
 
 <style scoped></style>
