09c0493045c3ac388cc2119fdfabdcf39a2f9572
Julian Zapata <julianandreszb@gmail.com>
2024-08-07 22:59:22 -0500

    Save progress: Project 3 - TicTacToe game

--- src/projects/TicTacToe/ToeBoard.vue
+++ src/projects/TicTacToe/ToeBoard.vue
@@ -1,6 +1,6 @@
 <script setup>
 import ToeBox from '@/projects/TicTacToe/ToeBox.vue'
-import { ref } from 'vue'
+import { reactive, ref } from 'vue'
 
 const combinations = [
   [0, 1, 2],
@@ -12,13 +12,44 @@ const combinations = [
   [3, 4, 5],
   [6, 7, 8]
 ]
-const currentPlayer = ref('X')
-const board = Array.from({ length: 9 }, (v, i) => {
-  return { index: i, player: null }
-})
-const isWinner = ref(false)
 
-function checkWin() {
+function fillBoard(board) {
+  Array.from({ length: 9 }, (v, i) => {
+    const box = { index: i, player: null }
+    board.push(box)
+    return box
+  })
+}
+function clearBoard(board) {
+  board.splice(0)
+}
+const board = reactive([])
+fillBoard(board)
+
+const currentPlayer = ref('X')
+const isWinner = ref(false)
+const isDraw = ref(false)
+const isNewGameButtonVisible = ref(false)
+
+function startNewGame() {
+  clearBoard(board)
+  fillBoard(board)
+  currentPlayer.value = 'X'
+  isDraw.value = false
+  isWinner.value = false
+  isNewGameButtonVisible.value = false
+}
+
+function isDrawGame() {
+  for (const box of board) {
+    if (box.player === null) {
+      return false
+    }
+  }
+  return true
+}
+
+function isWin() {
   for (const combination of combinations) {
     let hitsCounter = 0
     for (const index of combination) {
@@ -37,19 +68,26 @@ function checkWin() {
 
 function updateBox(box) {
   board[box.index].player = currentPlayer.value
-  if (checkWin()) {
-    console.log(`${currentPlayer.value} wins!`)
-    isWinner.value = true
-  } else {
-    currentPlayer.value = currentPlayer.value === 'X' ? 'O' : 'X'
+
+  isDraw.value = isDrawGame()
+  isWinner.value = isWin()
+
+  if (isDraw.value || isWinner.value) {
+    isNewGameButtonVisible.value = true
+    return
   }
+
+  currentPlayer.value = currentPlayer.value === 'X' ? 'O' : 'X'
 }
 </script>
 
 <template>
   <section>
     <h1>Tic-tac-toe</h1>
-    <p>{{ isWinner ? `Player ${currentPlayer} wins!` : `Player ${currentPlayer}'s turn` }}</p>
+    <p v-if="isWinner">{{ `Player ${currentPlayer} wins!` }}</p>
+    <p v-else-if="isDraw">{{ `Draw!` }}</p>
+    <p v-else>{{ `Player ${currentPlayer}'s turn` }}</p>
+    <button type="button" v-if="isNewGameButtonVisible" @click="startNewGame">New Game</button>
     <article>
       <div class="grid">
         <ToeBox
